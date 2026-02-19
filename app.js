82aab222fa5a79140527c5146dbafd3d84dae564
Sander <smk012026@gmail.com>
2026-02-19 14:38:18 +0100

    Lagt til løsninger frem til oppgave 3

--- app.js
+++ app.js
@@ -27,28 +27,46 @@ const når du definerer variablene.
 // Skriv koden for oppgave 2 her
 
 //Her løser jeg oppgaven med å lage varibler med datatypen String
-let fornavn = "Sander"
-let age = "28"
+{let fornavn = "Sander";
+let age = "28";
 
 let string = "Hei, mitt navn er " + fornavn + ", jeg er " + age + " år gammel."
-console.log(string)
+console.log(string);}
 
 // Her løser jeg oppgaven med å lage varibler med datatypen Number
 
-const num1 = 5
-const num2 = 8
-let sum = num1 + num2
-console.log (sum)
+{const num1 = 5;
+const num2 = 8;
+let sum = num1 + num2;
+console.log (sum);
 
-const num3 = 10
-const num4 = 4
-let sum2 = num3 + num4
-console.log(sum2)
+const num3 = 10;
+const num4 = 4;
+let sum2 = num3 + num4;
+console.log(sum2);
 
 //Totalsum
-sum += sum2
-console.log(sum)
+sum += sum2;
+console.log(sum);}
 
+//Her løser jeg oppgaven med datatypen Boolean
+
+{let isOnline = true;
+console.log("Is the user online? " + isOnline);
+
+let myAge = 18;
+let isAdult = myAge >= 18;
+
+console.log("Is this user an adult? " + isAdult);}
+
+
+//Her løser jeg oppgaven med datatypen Array
+
+{let myCars = ["Volvo", "Bmw", "Mercedes", "Toyota" ];
+console.log(myCars[2])
+
+let myNumbers = [1,2,3,4,5,6,7,8,9];
+console.log(myNumbers[4]);}
 
 
 /******************************************************************************
@@ -62,6 +80,25 @@ Skriv noen eksempler der du tester disse operatorene.
 ******************************************************************************/
 
 // Skriv koden for oppgave 3 her
+
+// med + operator
+
+{
+   const a = 5;
+   const b = 7;
+   let c = a + b;
+   console.log(c);
+}
+
+
+{
+    const a = 19;
+    const b = 11;
+    let c = a - b;
+    console.log(c);
+}
+
+
 
 /******************************************************************************
 OPPGAVE 4
