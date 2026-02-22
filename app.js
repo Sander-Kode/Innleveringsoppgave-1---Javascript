4bef66f80966b8387a0aebd5fa3fd79b87deae47
Sander <smk012026@gmail.com>
2026-02-22 19:00:38 +0100

    Oppdaterte oppgaver (hjemme-PC)

--- app.js
+++ app.js
@@ -27,46 +27,53 @@ const når du definerer variablene.
 // Skriv koden for oppgave 2 her
 
 //Her løser jeg oppgaven med å lage varibler med datatypen String
-{let fornavn = "Sander";
-let age = "28";
+{
+    let fornavn = "Sander";
+    let age = 28;
 
-let string = "Hei, mitt navn er " + fornavn + ", jeg er " + age + " år gammel."
-console.log(string);}
+    let string = "Hei, mitt navn er " + fornavn + ", jeg er " + age + " år gammel."
+    console.log(string);
+}
 
 // Her løser jeg oppgaven med å lage varibler med datatypen Number
 
-{const num1 = 5;
-const num2 = 8;
-let sum = num1 + num2;
-console.log (sum);
+{
+    const num1 = 5;
+    const num2 = 8;
+    let sum = num1 + num2;
+    console.log (sum);
 
-const num3 = 10;
-const num4 = 4;
-let sum2 = num3 + num4;
-console.log(sum2);
+    const num3 = 10;
+    const num4 = 4;
+    let sum2 = num3 + num4;
+    console.log(sum2);
 
 //Totalsum
-sum += sum2;
-console.log(sum);}
+    sum += sum2;
+    console.log(sum);
+}
 
 //Her løser jeg oppgaven med datatypen Boolean
 
-{let isOnline = true;
-console.log("Is the user online? " + isOnline);
+{
+    let isOnline = true;
+    console.log("Is the user online? " + isOnline);
 
-let myAge = 18;
-let isAdult = myAge >= 18;
+    let myAge = 18;
+    let isAdult = myAge >= 18;
 
-console.log("Is this user an adult? " + isAdult);}
+    console.log("Is this user an adult? " + isAdult);}
 
 
 //Her løser jeg oppgaven med datatypen Array
 
-{let myCars = ["Volvo", "Bmw", "Mercedes", "Toyota" ];
-console.log(myCars[2])
+{
+    let myCars = ["Volvo", "Bmw", "Mercedes", "Toyota" ];
+    console.log(myCars[2])
 
-let myNumbers = [1,2,3,4,5,6,7,8,9];
-console.log(myNumbers[4]);}
+    let myNumbers = [1,2,3,4,5,6,7,8,9];
+    console.log(myNumbers[4]);
+}
 
 
 /******************************************************************************
@@ -90,12 +97,65 @@ Skriv noen eksempler der du tester disse operatorene.
    console.log(c);
 }
 
-
+// med - operator
 {
     const a = 19;
     const b = 11;
     let c = a - b;
     console.log(c);
+}
+// med * operator
+
+{
+    const a = 9;
+    const b = 3;
+
+    console.log(a * b);
+}
+
+// med / operator
+{
+    const a = 8;
+    const b = 3;
+
+    console.log(a / b);
+
+}
+
+// med ++ operator
+{
+    let a = 5;
+    a++;
+    console.log(a);
+}
+
+// med -- operator
+
+{
+    let a = 10;
+    a--;
+
+    console.log(a);
+}
+
+// med += operator
+
+{
+    let a = 6;
+    let b = 4;
+
+    a += b;
+
+    console.log(a);
+}
+// med -= operator
+{
+    let a = 9;
+    let b = 4;
+
+    a -= b;
+
+    console.log(a);
 }
 
 
@@ -121,13 +181,24 @@ Prøv å endre verdiene på variablene for å sikre at IF/ELSE-setningen din
 håndterer alle tilfeller korrekt.
 ******************************************************************************/
 
-let userName = "";
+let userName = "Sander";
 let userAge = 18;
 let userIsLoggedIn = false;
 let userIsBlocked = false;
 let goToPage = "";
 
 // Skriv koden for oppgave 4 her
+
+if (userAge >= 18 && userName !== "" && userIsBlocked === false) {
+    userIsLoggedIn = true; 
+    goToPage = "/home";
+    console.log("Velkommen, du er nå logget inn.")
+} else {
+    console.log("error: brukeren oppfyller ikke kravene.")
+}
+
+
+
 
 /******************************************************************************
 OPPGAVE 5
@@ -147,3 +218,12 @@ at betingelsen din fungerer som den skal.
 const userMale = false;
 
 // Skriv koden for oppgave 5 her
+
+// Her sier jeg at usertitle = Mr. om userMale er true, men Mrs. om userMale er false.
+const userTitle = userMale ? "Mr." : "Mrs.";
+
+console.log(userTitle)
+
+
+//Tror jeg har svart på det meste, håper det er riktig løst :) 
+
